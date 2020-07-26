e430595bfbdd3313806d31e9dabde0b544f35614
Andre Immig <andre.immig@student.kit.edu>
2020-07-26 15:00:16 +0200

    Add some test cases for subnormal numbes (compare operators)

--- fbtor/test/test_set.py
+++ fbtor/test/test_set.py
@@ -8,9 +8,11 @@ from pyboolector import Boolector
 t = FPType.single
 d = FPType.double
 r = RMode.to_zero
+n = RMode.to_nearest
 
 set_fEq=  [ ("-0","+0",Boolector.SAT,t,r), ("-1","+1",Boolector.UNSAT,t,r), ("5e17","5e17",Boolector.SAT,t,r), ("5e17","5.000001e17",Boolector.UNSAT,t,r),
-           ("2.0000001","2",Boolector.SAT,t,r),("3.403E38","inf",Boolector.SAT,t,r),("3.4025E38","inf",Boolector.UNSAT,t,r)
+           ("2.0000001","2",Boolector.SAT,t,r),("3.403E38","inf",Boolector.SAT,t,r),("3.4025E38","inf",Boolector.UNSAT,t,r),
+           ("1.5e-45","0",Boolector.UNSAT,t,n),("1e-46","0",Boolector.SAT,t,n)
           ]
 
 set_fGtE= [ ("-0","+0",Boolector.SAT,t,r), ("-1","+1",Boolector.UNSAT,t,r), ("5e17","5e17",Boolector.SAT,t,r), ("5e17","5.000001e17",Boolector.UNSAT,t,r),
@@ -27,7 +29,8 @@ set_fGt=  [ ("-0","0",Boolector.UNSAT,d,r), ("-1","+1",Boolector.UNSAT,d,r), ("5
 
 set_fLt=  [ ("0","0",Boolector.UNSAT,t,r), ("-1","+1",Boolector.SAT,t,r), ("5e17","5e17",Boolector.UNSAT,t,r), ("5e17","5.000001e17",Boolector.SAT,t,r),
             ("2.0000001","2",Boolector.UNSAT,t,r), ("-1.797693e308","-inf",Boolector.UNSAT,d,r), ("-inf","-inf",Boolector.UNSAT,d,r),
-            ("-inf","inf",Boolector.SAT,d,r)
+            ("-inf","inf",Boolector.SAT,d,r),("3.724e-40","3.74e-40",Boolector.SAT,t,n),("1e-40","4",Boolector.SAT,t,n),
+            ("3.724e-40","-3.74e-40",Boolector.UNSAT,t,n),("1e-40","-4",Boolector.UNSAT,t,n)
           ]
 
 set_fSign=[ ('-inf',"1",t,r), ("-5.12e3","1",t,r), ("-2.17e-4","1",t,r), ("-0","1",t,r), ("-3.72E-40","1",t,r),
