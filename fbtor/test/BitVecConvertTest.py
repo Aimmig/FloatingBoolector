7d1ad023f1a78807d78487ed21d669f9f85385b7
Andre Immig <andre.immig@student.kit.edu>
2020-07-28 12:28:28 +0200

    Add some test for BitVecConvert

--- fbtor/test/BitVecConvertTest.py
+++ fbtor/test/BitVecConvertTest.py
@@ -1,12 +1,11 @@
 from fbtor.BitVecConvert import BitVecConvStatic
 from fbtor.FBoolectorTypes import FPType, RMode, WIDTH, MAN, EXP
+from .test_converter import *
 import pytest
 
 """
 Test cases for the string to bitstring implementation:
 """
-
-#TO-DO: Cover more cases .... maybe look at the other scripting file and check the output with an online converter
 
 def test_Zero():
      for t in FPType:
@@ -29,3 +28,19 @@ def test_Inf():
                    signbit ="1"
                 print(res)
                 assert res == (signbit + t.value[EXP]*"1"+ t.value[MAN]*"0")
+
+@pytest.mark.parametrize('inp,expected,fptype,rmode',subnormal)
+def test_subnormal(inp,expected, fptype,rmode):
+    assert BitVecConvStatic.convertToBinary(inp,fptype,rmode) == expected
+
+@pytest.mark.parametrize('inp,expected,fptype,rmode', small)
+def test_small(inp,expected, fptype,rmode):
+    assert BitVecConvStatic.convertToBinary(inp,fptype,rmode) == expected
+
+@pytest.mark.parametrize('inp,expected,fptype,rmode', large)
+def test_large(inp,expected, fptype,rmode):
+    assert BitVecConvStatic.convertToBinary(inp,fptype,rmode) == expected
+
+@pytest.mark.parametrize('inp,expected,fptype,rmode', normal)
+def test_normal(inp,expected, fptype,rmode):
+    assert BitVecConvStatic.convertToBinary(inp,fptype,rmode) == expected
