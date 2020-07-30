8eac472787f076f72aa57f8a106e9d6bb554cb19
Andre Immig <andre.immig@students.kit.edu>
2020-07-30 17:58:24 +0200

    Some more test cases

--- fbtor/BitVecConvert.py
+++ fbtor/BitVecConvert.py
@@ -3,7 +3,7 @@ from fbtor.FBoolectorTypes import FPType, RMode
 
 # TO-DO: look at remaining TO-DOs or remove them ...
 # TO-DO: remove this fixed bit-precision
-additional_bits = 5
+additional_bits = 10
 #additional_bits = 3
 """ Class that contains functionality to convert a decimal number to IEE-754 floating-point bitvector.
 
