4b9f999496d3dab3b768e47bab9369c5e1ca35ae
Andre Immig <andre.immig@students.kit.edu>
2020-08-13 19:15:28 +0200

    Fix example for quadratic equation

--- quadratic_equation_test.py
+++ quadratic_equation_test.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
-from fbtor.FBoolectorTypes import FPType, RMode, EXP, MAN, WIDTH
+#TO-DO: cleanup ....
+
+from fbtor.FBoolectorTypes import FPType, RMode, WIDTH
 from fbtor.FBoolector import FBoolector
 from pyboolector import BTOR_OPT_MODEL_GEN
 
 if __name__ == "__main__":
-    fptype = FPType.single
+    fptype = FPType.double
     rmode = RMode.to_zero
     
     # Try to solve a simple quadratic equation which is 0 at the following points
-    val1="-24"
+    val1="2"
     val2="256"
     
     # setup FBoolector, variables etc
@@ -21,17 +23,13 @@ if __name__ == "__main__":
     x2 = fbtor.fConst(val2)
     
     # First (simpler) approach is to use 0=(x-1)(x-x2)
-    formula1 = fbtor.fEq(fbtor.fConst("0"),
-                        fbtor.fMul(
-                              fbtor.fSub(x,x1),
-                              fbtor.fSub(x,x2)
-                        )
-              )
+    f = fbtor.fMul(fbtor.fSub(x,x1),fbtor.fSub(x,x2))
+    formula1 = fbtor.fEq(fbtor.fConst("0"),f)
     fbtor.Assert(formula1)
     
     #manually exclude x1 or x2, if both are excluded formula becomes UNSAT
-    #fbtor.Assert(fbtor.Not(fbtor.fEq(x,x1)))
-    #fbtor.Assert(fbtor.Not(fbtor.fEq(x,x2)))
+    fbtor.Assert(fbtor.Not(fbtor.fEq(x,x1)))
+    fbtor.Assert(fbtor.Not(fbtor.fEq(x,x2)))
     
     result = fbtor.Sat()
     
@@ -41,7 +39,6 @@ if __name__ == "__main__":
         print(x2.assignment)
         print("found:")
         print(x.assignment)
-        print(fbtor.fExponent(x).assignment)
         #fbtor.Print_model(format="smt2")
     elif(result == fbtor.UNSAT):
         print("1. approach: UNSAT")
@@ -55,22 +52,29 @@ if __name__ == "__main__":
     x = fbtor.fVar(s)
     x1 = fbtor.fConst(val1)
     x2 = fbtor.fConst(val2)
+    eps = fbtor.fConst("1e-25")
     
     #Now solve the same formula but in the form 0=(x^2 -(x1+x2)*x)+ x1*x2
-    formula2 = fbtor.fEq(fbtor.fConst("0"),
-                      fbtor.fAdd(
-                          fbtor.Sub(fbtor.fMul(x,x),
-                                    fbtor.fMul(fbtor.fAdd(x1,x2),x)
-                          ),
-                          fbtor.fMul(x1,x2)
-                      )
-                )
+    f = fbtor.fAdd(fbtor.fSub(fbtor.fMul(x,x),
+                              fbtor.fMul(fbtor.fAdd(x1,x2),x)
+                         ),
+                   fbtor.fMul(x1,x2)
+                  )
+    formula2 = fbtor.fEq(fbtor.fConst("0"),f)
     fbtor.Assert(formula2)
     
     #manually exclude x1 or x2, if both are excluded formula becomes UNSAT
     #fbtor.Assert(fbtor.Not(fbtor.fEq(x,x1)))
-    #fbtor.Assert(fbtor.Not(fbtor.fEq(x,x2)))
+    fbtor.Assert(fbtor.Not(fbtor.fEq(x,x2)))
     
+    #manually restrict search area
+    #fbtor.Assert(fbtor.fGt(x,fbtor.fAdd(fbtor.fConst("1e-5"),x2)))
+    #fbtor.Assert(fbtor.fLt(x,x1))
+    #fbtor.Assert(fbtor.And(fbtor.fGt(x,fbtor.fAdd(x1,eps)),fbtor.fLt(x,fbtor.fSub(x2,eps))))
+    
+    # TO-DO: NEU FORMULIEREN NACH FIX --------------------------------------------
+   
+    # THIS IS WRONG after the fix
     # NOTE: Formula2 is even UNSAT when not restricting the possible output values.
     # Our implementation is therefore not able to find the expected zero values
     # This means Formula1 is not satisfiablity equvivalent to Formula2 even
